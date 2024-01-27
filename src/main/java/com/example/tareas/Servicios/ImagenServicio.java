809ba751b6faa40b0ed937cd43f19e8030ef14cf
Gabriel Delgado <gabrieldelgado183@icloud.com>
2024-01-27 16:11:29 -0300

    probando commit

--- src/main/java/com/example/tareas/Servicios/ImagenServicio.java
+++ src/main/java/com/example/tareas/Servicios/ImagenServicio.java
@@ -2,13 +2,22 @@ package com.example.tareas.Servicios;
 
 import com.example.tareas.entidades.Imagen;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ImagenServicio {
+
+    @Autowired(required = true)
+
     public Imagen guardarImagen(MultipartFile archivo);
-    public Imagen actualizar(MultipartFile archivo,String idImagen);
-    public List<Imagen>listarImagen();
+
+    public Imagen actualizar(MultipartFile archivo, String idImagen);
+
+    public List<Imagen> listarImagen();
+
     public void borrarImagen(String idImagen);
-    public List<Imagen>guardarImagenLista(List<MultipartFile>archivos);
+
+    public List<Imagen> guardarImagenLista(List<MultipartFile> archivos);
+
     public Imagen getOne(String id);
 }
