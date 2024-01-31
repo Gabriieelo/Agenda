d00526b2b60fb8cc3aed21d3703747978c5d978e
Gabriel Delgado <gabrieldelgado183@icloud.com>
2024-01-31 18:18:31 -0300

    segundo commit

--- src/main/java/com/example/tareas/controlador/ImagenControlador.java
+++ src/main/java/com/example/tareas/controlador/ImagenControlador.java
@@ -1,7 +1,6 @@
 package com.example.tareas.controlador;
 
 import com.example.tareas.Servicios.ImagenServicio;
-import com.example.tareas.Servicios.UsuarioServicio;
 import com.example.tareas.entidades.Imagen;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -16,14 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/imagen")
 public class ImagenControlador {
-    @Autowired 
-    private UsuarioServicio us;
     @Autowired
-    private ImagenServicio is;
+    private ImagenServicio imagenServicio;
     @GetMapping("/vista/{id}")
     public ResponseEntity<byte[]> imagenUsuario(@PathVariable String id){
         //Usuario usuario = usuarioServicio.getOne(id);
-        Imagen archivo =is.getOne(id);
+        Imagen archivo =imagenServicio.getOne(id);
         byte[] imagen = archivo.getContenido();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.IMAGE_JPEG);
